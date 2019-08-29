89979ea8a6bdcd506ce8dc42647ff5705b77f5be
HP <HP@192.168.110.149>
2019-08-29 17:32:59 +0800

    1

--- spring_fx/src/main/java/controller/test.java
+++ spring_fx/src/main/java/controller/test.java
@@ -3,13 +3,12 @@ package controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class test {
 
 	@RequestMapping("index")
-	public void index(@RequestParam String name ,ModelMap m) {
+	public void index(String name ,ModelMap m) {
 		m.put("name", name);
 		
 	}
