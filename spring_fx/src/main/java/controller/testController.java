75a566e4b129268991e697a66bfbb352a5a27deb
HP <HP@192.168.110.149>
2019-09-02 09:05:02 +0800

    1

--- spring_fx/src/main/java/controller/testController.java
+++ spring_fx/src/main/java/controller/testController.java
@@ -1,14 +1,9 @@
 package controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import entity.Book;
 import entity.Type;
 import service.Book_Service;
 import service.Type_Service;
